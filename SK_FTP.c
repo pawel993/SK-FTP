757690c6acd62fa697720a25b7c76f3e74f38eda
root <root@linux-joby.site>
2015-01-05 23:23:32 +0100

    dzialajace mkdir i poprawki do obslugi komend

--- SK_FTP.c
+++ SK_FTP.c
@@ -11,50 +11,127 @@
 
 #define QSIZE 5
 #define BUFSIZE 10000
-
+char* current_dir;
+char* mode="ACTIVE";
 char* r_220="220 Service ready for new user.\n";
-char* r_215="215 Unix sytem type.\n";
+char* r_215="215 Unix system type.\n";
 char* r_331="331 User name okay, need password.\n";
 char* r_230="230 User logged in, proceed.\n";
 char* r_200="200 Command okay.\n";
 char* r_500="500 Syntax error, command unrecognized.\n";
 char* r_257="257 /usr/Xanril/ directory.\n";
-char* r_227="227 Entering Passive Mode (h1,h2,h3,h4,p1,p2).\n";
+char* r_227="227 Entering Passive Mode (127,0,0,1,4,21).\n";
 char* r_502="502 Command not implemented.\n";
+char* r_150="150 File status okay; about to open data connection.\n";
+char* r_125="125 Data connection already open; transfer starting.\n";
+char* r_226="226 Closing data connection.\n";
 
 char *protocol = "tcp";
 
 ushort service_port = 21;
+ushort passive_port = 1045;
 ushort data_port = 20;
 
 pthread_t main_thread;
 
-void serv_command(void* arg)
+
+
+void create_dir(char* directory_name)
 {
-char command_buf[256];  
-read((int) arg,command_buf,256);
-strcat(command_buf," > res.txt");
-printf("%s\n",command_buf);
-system(command_buf);
-FILE *fp;
-fp = fopen("res.txt", "r"); 
+ printf("Directory %s create\n",directory_name);
+ char cmd[6]="mkdir ";
+ strcat(cmd,directory_name);
+ printf("%s",cmd);
+ system(cmd);
+}
+
+void ls(void* arg)
+{
+system("ls -l > res.txt");
+FILE* fp;
+fp = fopen("res.txt", "rb"); 
   if(NULL == fp)
   {
   printf("Error opening file");
   exit(EXIT_FAILURE);
   } 
-while(1)
+if(strcmp(mode,"PASSIVE")==0)
 {
-unsigned char buff[256]={0};
-int nread = fread(buff,1,256,fp);    
+  printf(" PASSIVE MODE LS\n");
+  struct sockaddr_in serv_data,client_data;
+  int data_sck,cli_data,cli_len;
+  bzero(&serv_data, sizeof serv_data);
+  serv_data.sin_addr.s_addr = INADDR_ANY;
+  serv_data.sin_family= AF_INET;
+  serv_data.sin_port = htons(passive_port);
+  
+  if ((data_sck = socket (PF_INET,SOCK_STREAM,IPPROTO_TCP))<0){
+    perror("Cannot open socket");
+    exit(EXIT_FAILURE);
+  }
+ 
+ 
+  if (bind(data_sck,(struct sockaddr*)&serv_data, sizeof serv_data) <0){
+    printf("Cannot bind socket %d to %d port\n",data_sck,passive_port);
+    exit(EXIT_FAILURE);
+  }
+  write((int) arg,r_150,strlen(r_150));printf(" Respons 150\n");
+  
+  if (listen (data_sck,QSIZE)<0){
+    perror("Cannot listen");
+    exit(EXIT_FAILURE);
+  }
+  
+  
+  printf("Waiting...\n");
+  
+  cli_len = sizeof (struct sockaddr_in);
+  if ((cli_data = accept(data_sck,(struct sockaddr*) &client_data, (socklen_t*) &cli_len)) < 0){
+    perror("Error while connecting with client");
+    exit(EXIT_FAILURE);
+  }  
+    printf("Data connection established\n");
+  while(1)
+  {
+  unsigned char buff[256]={0};
+  int nread = fread(buff,8,256,fp);    
+  if(nread > 0)
+  {        
+  printf("%s\n",buff);
+  write(cli_data, buff, nread);	
+  }
+  if(nread <256)break;
+  }
+  close((int)fp);
+  write((int) arg,r_226,strlen(r_226));
+  close(data_sck);
+  close(cli_data);
+  }
+else
+{
+}
+}
 
-if(nread > 0)
-{                           
-write((int) arg, buff, nread);
+void addr(char* addres)
+{
+char temp[48];
+char* port;
+int i=0;
+int count=0;
+int count2=0;
+strncpy(temp,addres,48);
+for(i=0;i<strlen(addres)-1;i++)
+{
+if(temp[i]==44)
+{
+count++;
+temp[i]=46;
 }
-if(nread <256)break;
+if(count<4){count2++;}
 }
-close((int)fp);
+char ad[count2];
+strncpy(ad,temp,count2);
+printf("%s   ||\n",ad);
 }
 
 int respond(void* arg)
@@ -64,16 +141,29 @@ char* command;
 char* atribut;
 int status=0;
 read((int) arg,buffer,256);
+printf("%s",buffer);
 command=strtok(buffer," ");
 atribut=strtok(NULL," ");
-if(strcmp(command,"USER")!=0 && strcmp(command,"PORT")!=0){command[strlen(command)-2]='\0';printf("%s",command);}
-if(strcmp(command,"USER")==0){write((int) arg,r_230,strlen(r_230));printf("%s Response 230\n",command);}
-else if(strcmp(command,"PORT")==0){write((int) arg,r_200,strlen(r_200));printf("%s Response 200\n",command);}
-else if(strcmp(command,"SYST")==0){write((int) arg,r_215,strlen(r_215));printf(" Respons 215\n");}
-else if(strcmp(command,"PWD")==0){write((int) arg,r_257,strlen(r_257));printf(" Respons 257\n");}
-else if(strcmp(command,"PASV")==0){write((int) arg,r_227,strlen(r_227));printf(" Respons 227\n");}
-else if(strcmp(command,"QUIT")==0){status=1;printf(" Respons quit\n");}
-else if(strcmp(command,"LIST")==0){write((int) arg,r_502,strlen(r_502));printf(" Respons 502\n");}
+if(strcmp(command,"USER")!=0 && strcmp(command,"PORT")!=0 && strcmp(command,"MKD")!=0 && strcmp(command,"TYPE")!=0 )
+{command[strlen(command)-2]='\0';printf("%s",command);}
+if(strcmp(command,"USER")==0)
+{write((int) arg,r_230,strlen(r_230));printf("%s Response 230\n",command);}
+else if(strcmp(command,"MKD")==0)
+{atribut[strlen(atribut)-2]='\0';create_dir(atribut);write((int) arg,r_257,strlen(r_257));printf("%s %s Response 257\n",command,atribut);}
+else if(strcmp(command,"PORT")==0)
+{atribut[strlen(atribut)-2]='\0';write((int) arg,r_200,strlen(r_200));printf("%s Response 200\n",command);addr(atribut);}
+else if(strcmp(command,"SYST")==0)
+{write((int) arg,r_215,strlen(r_215));printf(" Respons 215\n");}
+else if(strcmp(command,"PWD")==0)
+{write((int) arg,r_257,strlen(r_257));printf(" Respons 257\n");}
+else if(strcmp(command,"PASV")==0)
+{write((int) arg,r_227,strlen(r_227));printf(" Respons 227\n");mode="PASSIVE";ls(arg);}
+else if(strcmp(command,"QUIT")==0)
+{status=1;printf(" Respons quit\n");}
+else if(strcmp(command,"LIST")==0)
+{ls(arg);}
+else if(strcmp(command,"TYPE")==0)
+{write((int) arg,r_200,strlen(r_200)),printf(" Respons 200\n");}
 else {write((int) arg,r_500,strlen(r_500));printf(" Response 500\n");}
 return status;
 }
