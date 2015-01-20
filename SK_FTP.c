4d25af127104ce9ddaf4de2d9a44e41f5393d015
root <root@linux-joby.site>
2015-01-20 21:10:04 +0100

    Wielowatkowosc i komenda cd

--- SK_FTP.c
+++ SK_FTP.c
@@ -36,29 +36,38 @@ pthread_t main_thread;
 // Przesylanie danych
 //#################################################
 
-void send_file(char* file_name,void* arg,char* mode)
+void send_file(char* file_name,int arg,char* mode)
 {
+int bytesReceived;
+char recvBuff[256];
 FILE *fp;
-fp = fopen(file_name, "rb");
+fp = fopen(file_name, mode);
 if(NULL == fp)
 {
 printf("Error opening file");
 exit(EXIT_FAILURE);
 }
-while(1)
+if(strcmp(mode,"rb+")==0)
 {
-unsigned char buff[256]={0};
-int nread = fread(buff,1,256,fp);
-if(nread > 0)
-{
-write((int) arg, buff, nread);
+
+ while((bytesReceived = fread(recvBuff,1,256,fp))>0)
+    {
+        printf("%s %d\n",recvBuff,bytesReceived);
+        write(arg,recvBuff,bytesReceived);
+    }
 }
-if (nread < 256)
+else
 {
-break;
-}
+  while(fgets(recvBuff, 256,fp)!=NULL)
+    {
+        printf("%s\n",recvBuff);
+        write(arg,recvBuff,strlen(recvBuff));
+    }
 }
 close((int)fp);
+
+
+
 }
 
 void recive_file(char* file_name,int arg,char* mode)
@@ -85,7 +94,8 @@ else
 {
   while((bytesReceived = read(arg, recvBuff, 256)) > 0)
     {
-      fputs(recvBuff,fp);
+        printf("%s %d\n",recvBuff,bytesReceived);
+        fputs(recvBuff,fp);
     }
 }
 close((int)fp);
@@ -135,26 +145,24 @@ if(temp[c]=='.')temp[c]=',';
 sprintf(result,"(%s,%d,%d).\n",temp,r1,r2);
 }
 
-void addr(char* addr)
+int addr(char* addr,char* result)
 {
 int i;
+int port_r;
 int count=0;
 char addres[256];
-char* add;
 char* port;
 char* port2;
 strncpy(addres,addr,strlen(addr));
-printf("%s %d\n",addr,strlen(addr));
 for(i=0;i<strlen(addres);i++){
 if(addres[i]==','){count++;}
 if(count<4 && addres[i]==','){addres[i]='.';}
 }
-printf("%s\n",addres);
-add=strtok(addres,",");
+result=strtok(addres,",");
 port=strtok(NULL,",");
 port2=strtok(NULL,",");
-printf("%s %s %s\n",add,port,port2);
-num_to_port(atoi(port),atoi(port2));
+port_r=num_to_port(atoi(port),atoi(port2));
+return port_r;
 }
 
 static void capture(int signo){
@@ -172,29 +180,14 @@ return passive_p;
 //Funkcje serwera
 //#########################################################
 
-void pwd(char path[256])
+void ls(int arg,char current_path[256])
 {
-getcwd(path,256);
-}
-
-void create_dir(char* directory_name,void* arg)
-{
-char cmd[50];
-sprintf(cmd,"mkdir %s",directory_name);
-char r_257[256];
-char temp[256];
-pwd(temp);
+char cmd[256];
+sprintf(cmd,"ls -l %s >> res.txt",current_path);
 system(cmd);
-sprintf(r_257,"257 %s/%s created.\n",temp,directory_name);
-write((int) arg,r_257,strlen(r_257));
-}
-
-void ls(int arg)
-{
-system("ls -l > res.txt");
 FILE *fp;
 char wiersz[256];
-fp = fopen("res.txt", "rb");
+fp = fopen("res.txt", "rt");
 if(NULL == fp)
 {
 printf("Error opening file");
@@ -244,8 +237,36 @@ dd[1]=cli_data;
 printf("Data connection established\n");
 }
 
-void active_connection(char* address,char* port)
+void active_connection(char* address,ushort port,int dd[2])
 {
+  int data_sck;
+struct sockaddr_in serv_data,client_data;
+  int serv_port=passive_port();
+bzero(&serv_data, sizeof serv_data);
+serv_data.sin_addr.s_addr = INADDR_ANY;
+serv_data.sin_family= AF_INET;
+serv_data.sin_port = htons(serv_port);
+
+bzero(&client_data, sizeof client_data);
+client_data.sin_addr.s_addr = inet_addr("127.0.0.1");
+client_data.sin_family= AF_INET;
+client_data.sin_port = htons(port);
+
+if ((data_sck = socket (PF_INET,SOCK_STREAM,IPPROTO_TCP))<0){
+perror("Cannot open socket");
+exit(EXIT_FAILURE);
+}
+dd[0]=data_sck;
+if (bind(data_sck,(struct sockaddr*)&serv_data, sizeof serv_data) <0){
+printf("Cannot bind socket %d to %d port\n",data_sck,serv_port);
+exit(EXIT_FAILURE);}
+
+ if(connect(data_sck, (struct sockaddr *)&client_data, sizeof(client_data))<0)
+    {
+        printf("\n Error : Connect Failed \n");
+        exit(EXIT_FAILURE);
+    }
+dd[1]=data_sck;
 }
 
 
@@ -253,9 +274,10 @@ void active_connection(char* address,char* port)
 //Odpowiadanie na zadania klienta
 //##################################################
 
-int respond(void* arg)
+int respond(void* arg,char current_path[256])
 {
-char* mode="ab+";
+char* mode="rb+";
+char* mode_w="wb+";
 int desc[2];
 char buffer[256]=" ";
 char* command;
@@ -266,18 +288,33 @@ printf("%s",buffer);
 command=strtok(buffer," ");
 atribut=strtok(NULL," ");
 if(strcmp(command,"USER")!=0 && strcmp(command,"PORT")!=0 && strcmp(command,"MKD")!=0
- && strcmp(command,"TYPE")!=0 &&  strcmp(command,"RETR")!=0 &&  strcmp(command,"STOR")!=0 )
+ && strcmp(command,"TYPE")!=0 &&  strcmp(command,"RETR")!=0 &&  strcmp(command,"STOR")!=0 && strcmp(command,"CWD")!=0)
 {
 command[strlen(command)-2]='\0';printf("%s",command);
 }
 if(strcmp(command,"USER")==0)
 {
 write((int) arg,r_230,strlen(r_230));printf("%s Response 230\n",command);}
+else if(strcmp(command,"CWD")==0)
+{
+atribut[strlen(atribut)-2]='\0';
+if(strcmp(atribut,"..")==0)
+{
+int i;
+for(i=strlen(current_path);i>=0;i--)
+{
+if(current_path[i]=='/'){current_path[i]='\0';break;}
+}
+
+
+}
+else {sprintf(current_path,"%s/%s",current_path,atribut);}
+write((int) arg,r_200,strlen(r_200));printf("%s Response 200\n",command);}
 else if(strcmp(command,"STOR")==0)
 {
 atribut[strlen(atribut)-2]='\0';
 write((int) arg,r_150,strlen(r_150));
-recive_file(atribut,desc[1],mode);
+recive_file(atribut,desc[1],mode_w);
 write((int) arg,r_226,strlen(r_226));
 close(desc[1]);
 close(desc[0]);
@@ -285,23 +322,36 @@ printf("%s Response 226\n",command);
 }
 else if(strcmp(command,"RETR")==0)
 {
-atribut[strlen(atribut)-2]='\0';send_file(atribut,arg,mode);
+atribut[strlen(atribut)-2]='\0';
+write((int) arg,r_150,strlen(r_150));
+send_file(atribut,desc[1],mode);
+write((int) arg,r_226,strlen(r_226));
+close(desc[1]);
+close(desc[0]);
+printf("%s Response 226\n",command);
 }
 else if(strcmp(command,"MKD")==0)
 {
-atribut[strlen(atribut)-2]='\0';create_dir(atribut,arg);printf("%s %s Response 257\n",command,atribut);}
+atribut[strlen(atribut)-2]='\0';
+char cmd[50];
+sprintf(cmd,"mkdir %s/%s",current_path,atribut);
+char r_257[256];
+system(cmd);
+sprintf(r_257,"257 %s/%s created.\n",current_path,atribut);
+write((int) arg,r_257,strlen(r_257));
+printf("%s %s Response 257\n",command,atribut);}
 else if(strcmp(command,"PORT")==0)
 {
-atribut[strlen(atribut)-2]='\0';addr(atribut);write((int) arg,r_200,strlen(r_200));printf("%s Response 200\n",command);}
+char* address="";
+int port=0;
+atribut[strlen(atribut)-2]='\0';port=addr(atribut,address);active_connection("127.0.0.1",port,desc);write((int) arg,r_200,strlen(r_200));printf("%s Response 200\n",command);}
 else if(strcmp(command,"SYST")==0)
 {
 write((int) arg,r_215,strlen(r_215));printf(" Respons 215\n");}
 else if(strcmp(command,"PWD")==0)
 {
 char r_257[256];
-char temp[256];
-pwd(temp);
-sprintf(r_257,"257 %s current working directory.\n",temp);
+sprintf(r_257,"257 %s current working directory.\n",current_path);
 write((int) arg,r_257,strlen(r_257));printf(" Respons 257\n");}
 else if(strcmp(command,"PASV")==0)
 {
@@ -316,13 +366,13 @@ else if(strcmp(command,"QUIT")==0)
 status=1;printf(" Respons quit\n");}
 else if(strcmp(command,"LIST")==0)
 {
-write((int) arg,r_150,strlen(r_150));printf(" Respons 150\n");ls(desc[1]);
+write((int) arg,r_150,strlen(r_150));printf(" Respons 150\n");ls(desc[1],current_path);
 write((int)arg,r_226,strlen(r_226));close(desc[0]);close(desc[1]);}
 else if(strcmp(command,"TYPE")==0)
 {
 atribut[strlen(atribut)-2]='\0';
-if(strcmp(atribut,"A")==0){mode="wt+";}
-else {mode="wb+";}
+if(strcmp(atribut,"A")==0){mode="rt+";mode_w="wt+";}
+else {mode="rb+";mode_w="wb+";}
 write((int) arg,r_200,strlen(r_200)),printf("%s Respons 200\n",command);}
 
 else {write((int) arg,r_500,strlen(r_500));printf(" Response 500\n");}
@@ -336,9 +386,11 @@ void* function(void* arg)
 int stat=0;
 printf("Connection established..\n");
 write((int) arg,r_220,strlen(r_220));
+char current_path[256];
+getcwd(current_path,256);
 while(stat!=1)
 {
-stat=respond(arg);
+stat=respond(arg,current_path);
 }
 close((int) arg);
 return 0;
